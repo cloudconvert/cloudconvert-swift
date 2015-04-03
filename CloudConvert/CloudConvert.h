677757a272b46890a5a6564e3dcd5e5e6a7a3229
Josias Montag <josias@montag.info>
2015-04-03 17:19:15 +0200

    Initial Commit

---
+++ CloudConvert/CloudConvert.h
@@ -0,0 +1,19 @@
+//
+//  CloudConvert.h
+//  CloudConvert
+//
+//  Created by Josias Montag on 03.04.15.
+//  Copyright (c) 2015 Lunaweb Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CloudConvert.
+FOUNDATION_EXPORT double CloudConvertVersionNumber;
+
+//! Project version string for CloudConvert.
+FOUNDATION_EXPORT const unsigned char CloudConvertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudConvert/PublicHeader.h>
+
+
---
+++ CloudConvertTests/CloudConvertTests.swift
@@ -0,0 +1,36 @@
+//
+//  CloudConvertTests.swift
+//  CloudConvertTests
+//
+//  Created by Josias Montag on 03.04.15.
+//  Copyright (c) 2015 Lunaweb Ltd. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class CloudConvertTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
